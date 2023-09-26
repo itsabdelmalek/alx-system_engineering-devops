83055ee27ce3dabb7be4336511c0d1ff9be53b00
abdelmalek <abdelmalek@gmail.com>
2023-09-26 06:40:01 -0700

    task 5

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,51 +1,40 @@
-# webserver_manifest.pp
+# Install and configure Nginx
+class nginx {
+  package { 'nginx':
+    ensure => 'installed',
+  }
 
-# Install Nginx package
-package { 'nginx':
-  ensure => 'installed',
-}
+  file { '/var/www/html/index.html':
+    content => 'Hello World!',
+    require => Package['nginx'],
+  }
 
-# Enable and start Nginx service
-service { 'nginx':
-  ensure => 'running',
-  enable => true,
-}
-
-# Configure Nginx for root page
-file { '/etc/nginx/sites-available/default':
-  content => "
-  server {
+  file { '/etc/nginx/sites-available/default':
+    content => "
+server {
     listen 80 default_server;
     listen [::]:80 default_server;
-    root /var/www/html;
-    index index.html index.htm;
-    location / {
-      return 200 'Hello World!';
-    }
-  }",
-  require => Package['nginx'],
-  notify  => Service['nginx'],
-}
 
-# Configure Nginx for redirection
-file { '/etc/nginx/sites-available/redirect_me':
-  content => "
-  server {
-    listen 80;
-    listen [::]:80 default_server;
     root /var/www/html;
-    index index.html index.htm;
+    index index.html;
+
     location /redirect_me {
-      return 301 https://www.youtube.com/watch?v=dQw4w9WgXcQ;
+        return 301 https://www.youtube.com/watch?v=dQw4w9WgXcQ;
     }
-  }",
-  require => Package['nginx'],
-  notify  => Service['nginx'],
+
+    location / {
+        return 200 'Hello World!';
+    }
+}",
+    require => Package['nginx'],
+  }
+
+  service { 'nginx':
+    ensure => 'running',
+    enable => true,
+    require => File['/etc/nginx/sites-available/default'],
+  }
 }
 
-file { '/etc/nginx/sites-enabled/redirect_me':
-  ensure  => 'link',
-  target  => '/etc/nginx/sites-available/redirect_me',
-  require => File['/etc/nginx/sites-available/redirect_me'],
-  notify  => Service['nginx'],
-}
+# Apply the nginx class
+include nginx
