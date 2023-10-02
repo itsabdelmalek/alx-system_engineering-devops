523f52900f28dab3353bfcd0bd9b0fc35e82fd67
abdelmalek <abdelmalek@gmail.com>
2023-10-02 07:40:00 -0700

    fix

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,37 +1,41 @@
-# 2-puppet_custom_http_response_header.pp
-
-# Define a class that sets up the custom HTTP response header
-class custom_header {
-  
-  # Define the custom header name and value
-  $header_name = 'X-Served-By'
-  $header_value = $::hostname
-
-  # Install Nginx
+# Install and configure Nginx
+class nginx {
   package { 'nginx':
-    ensure => installed,
+    ensure => 'installed',
   }
 
-  # Create a custom Nginx config file with the desired header
-  file { '/etc/nginx/sites-available/custom_header':
-    ensure  => present,
-    content => "server {\n\tlisten 80;\n\tserver_name _;\n\n\tlocation / {\n\t\tadd_header $header_name $header_value;\n\t\treturn 200 'Hello, World!';\n\t}\n}",
+  file { '/var/www/html/index.html':
+    content => 'Hello World!',
+    require => Package['nginx'],
   }
 
-  # Create a symlink to enable the config
-  file { '/etc/nginx/sites-enabled/custom_header':
-    ensure => link,
-    target => '/etc/nginx/sites-available/custom_header',
-    require => File['/etc/nginx/sites-available/custom_header'],
+  file { '/etc/nginx/sites-available/default':
+    content => "
+server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+
+    root /var/www/html;
+    index index.html;
+
+    location /redirect_me {
+        return 301 https://www.youtube.com/watch?v=dQw4w9WgXcQ;
+    }
+
+    location / {
+        add_header X-Served-By $hostname;
+        return 200 'Hello World!';
+    }
+}",
+    require => Package['nginx'],
   }
 
-  # Restart Nginx
   service { 'nginx':
-    ensure    => running,
-    enable    => true,
-    subscribe => File['/etc/nginx/sites-enabled/custom_header'],
+    ensure => 'running',
+    enable => true,
+    require => File['/etc/nginx/sites-available/default'],
   }
 }
 
-# Include the class to apply the configuration
-include custom_header
+# Apply the nginx class
+include nginx
