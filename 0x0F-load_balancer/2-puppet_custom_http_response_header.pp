3d824817afbe47f385c5dbbe00d5ee52fc8e5669
abdelmalek <abdelmalek@gmail.com>
2023-10-02 13:13:36 -0700

    advanced

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,41 +1,20 @@
-# Install and configure Nginx
-class nginx {
-  package { 'nginx':
-    ensure => 'installed',
-  }
-
-  file { '/var/www/html/index.html':
-    content => 'Hello World!',
-    require => Package['nginx'],
-  }
-
-  file { '/etc/nginx/sites-available/default':
-    content => "
-server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-
-    root /var/www/html;
-    index index.html;
-
-    location /redirect_me {
-        return 301 https://www.youtube.com/watch?v=dQw4w9WgXcQ;
-    }
-
-    location / {
-        add_header X-Served-By $hostname;
-        return 200 'Hello World!';
-    }
-}",
-    require => Package['nginx'],
-  }
-
-  service { 'nginx':
-    ensure => 'running',
-    enable => true,
-    require => File['/etc/nginx/sites-available/default'],
-  }
+# installs and configures nginx
+exec { 'update':
+  command     => 'sudo apt-get -y update',
+  provider => shell,
 }
 
-# Apply the nginx class
-include nginx
+-> exec { 'install':
+  command => 'sudo apt-get -y install nginx',
+  provider => shell,
+}
+
+-> exec { 'replace':
+  command => 'sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-enabled/default',
+  provider => shell,
+}
+
+-> exec { 'restart':
+  command => 'sudo service nginx restart',
+  provider => shell,
+}
