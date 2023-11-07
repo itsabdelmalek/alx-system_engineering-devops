c9a59228005558ff6377a2b26503a14b083f6398
abdelmalek <abdelmalek@gmail.com>
2023-11-07 02:39:09 -0800

    fix

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,57 +1,61 @@
 #!/usr/bin/python3
-
 """
 This module contains a recursive function that queries the Reddit API,
-parses the titles of hot articles, and prints a sorted count of given keywords.
+parses the titles of hot articles, and prints a sorted count of given keywords
 """
-
 import requests
 
-def count_words(subreddit, word_list, count_dict={}, after=""):
+
+def count_words(subreddit, word_list, count_dict={}, after="", count=0):
     """
     Queries Reddit API, parses titles, and counts keywords.
 
     Args:
         subreddit (str): The name of the subreddit.
         word_list (list): List of keywords to count.
-        count_dict (dict, optional): Dictionary to store counts. Defaults to {}.
+        count_dict (dict, optional): Dictionary to store counts. Defaults to
         after (str, optional): A token for pagination. Defaults to "".
 
     Returns:
         dict: Dictionary containing counts of keywords.
     """
-    url = f"https://www.reddit.com/r/{subreddit}/hot/.json"
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
         "User-Agent": "my_script:reddit_api:v1.0.0 (by /u/Nickname-Pending1)"
     }
     params = {
         "after": after,
+        "count": count,
         "limit": 100
     }
-    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
-    
-    if response.status_code == 404:
-        return count_dict
+    resp = requests.get(url, headers=headers, params=params,
+                        allow_redirects=False)
+    try:
+        result = resp.json()
+        if resp.status_code == 404:
+            raise Exception
+    except Exception:
+        print("")
+        return
 
-    data = response.json().get("data")
-    after = data.get("after")
-    for child in data.get("children"):
-        title = child.get("data").get("title").lower()
+    result = result.get("data")
+    after = result.get("after")
+    count += result.get("dist")
+    for c in result.get("children"):
+        title = c.get("data").get("title").lower().split()
         for word in word_list:
-            if title.count(word.lower()) > 0:
-                count_dict[word.lower()] = count_dict.get(word.lower(), 0) + title.count(word.lower())
+            if word.lower() in title:
+                times = len([t for t in title if t == word.lower()])
+                if count_dict.get(word) is None:
+                    count_dict[word] = times
+                else:
+                    count_dict[word] += times
 
-    if after is not None:
-        return count_words(subreddit, word_list, count_dict, after)
-    return count_dict
-
-if __name__ == "__main__":
-    subreddit_name = "learnpython"  # Replace with the subreddit you want to check
-    keywords = ["python", "java", "javascript", "cpp", "c#"]  # Replace with your list of keywords
-    count_dict = count_words(subreddit_name, keywords)
-    if count_dict:
-        sorted_counts = sorted(count_dict.items(), key=lambda x: (-x[1], x[0]))
-        for keyword, count in sorted_counts:
-            print(f"{keyword}: {count}")
+    if after is None:
+        if len(count_dict) == 0:
+            print("")
+            return
+        count_dict = sorted(count_dict.items(), key=lambda kv: (-kv[1], kv[0]))
+        [print("{}: {}".format(k, v)) for k, v in count_dict]
     else:
-        print("No posts match or the subreddit is invalid.")
+        count_words(subreddit, word_list, count_dict, after, count)
